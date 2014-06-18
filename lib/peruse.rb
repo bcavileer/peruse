6daa591c80092a9314edceea0271917971ffb954
Benjamin Cavileer <bcavileer@holmanauto.com>
2014-06-18 14:59:56 -0400

    Refactor code - same behavior

--- lib/peruse.rb
+++ lib/peruse.rb
@@ -7,9 +7,10 @@ module Peruse
     def initialize(gemname, kernel = Kernel)
       @gemname = gemname
       @kernel = kernel
+      @root = @require_level = Tree::TreeNode.new(gemname)
     end
 
-    attr_accessor :gemname, :require_level, :root
+    attr_accessor :gemname, :require_level, :root, :current_name
     attr_reader :kernel
 
     def peruse
@@ -20,53 +21,67 @@ module Peruse
 
     def record
       set_recorder
-      monkey_kernel do
-        copy_original_kernel_require
-        redefine_kernel_require
-        yield
-        restore_original_kernel_require
-      end
+      copy_original_kernel_require
+      redefine_kernel_require
+      yield
+      restore_original_kernel_require
       require_level
     end
 
-    def restore_original_kernel_require
+    private
+
+    def set_recorder
+      $require_recorder = self
+    end
+
+    def copy_original_kernel_require
       kernel.class_eval do
-        alias :require :orig_require
+        alias :original_kernel_require :require
       end
     end
 
     def redefine_kernel_require
       kernel.class_eval do
-        def require(file)
-          $recorder.instance_eval do
-            self.require_level = if require_level
-                                   require_level[file] or require_level << Tree::TreeNode.new(file)
-                                 else
-                                   Tree::TreeNode.new(file)
-                                 end
-            self.root ||= require_level
-            r = orig_require(file)
-            self.require_level = require_level.parent || root
+        def require(name)
+          $require_recorder.instance_eval do
+            set_current_name(name)
+            push_require_level unless current_level?
+            r = original_kernel_require(name)
+            pop_require_level
             r
           end
         end
       end
     end
 
-    def copy_original_kernel_require
-      kernel.class_eval do
-        alias :orig_require :require
-      end
+    def set_current_name(name)
+      self.current_name = name
     end
 
-    def monkey_kernel
-      kernel.class_eval do
-        yield
-      end
+    def current_level?
+      require_level.name == current_name
     end
 
-    def set_recorder
-      $recorder = self
+    def push_require_level
+      self.require_level = existing_child || new_child
+    end
+
+    def existing_child
+      require_level[current_name]
+    end
+
+    def new_child
+      require_level << Tree::TreeNode.new(current_name)
+    end
+
+    def pop_require_level
+      self.require_level = require_level.parent || root
+    end
+
+    def restore_original_kernel_require
+      kernel.class_eval do
+        alias :require :original_kernel_require
+      end
     end
   end
 end
